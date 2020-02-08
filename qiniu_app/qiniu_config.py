1b93085632b3281278a18f0c03c3bb28d511be83
xsan777 <1315969544@qq.com>
2020-02-08 11:35:51 +0800

    修改

--- qiniu_app/qiniu_config.py
+++ qiniu_app/qiniu_config.py
@@ -1,6 +1,8 @@
 qiniu_config = {
-    'access_key': 'm5X1CxO2PGwoK4bN0Oxo8uUaPJoT7dsZj10qRKSD',
-    'secret_key': 'cSrD5f6E__JXTZgOZsDRLZUZc_NpFrdB_XHQo380',
-    'bucket_name': 'xsan',
-    'domine': 'http://img.xiaobaods.com/',
+    'access_key': '',
+    'secret_key': '',
+    'bucket_name': '',
+    'domine': '',
 }
+# bucket_name：空间名
+# domine：空间域名
